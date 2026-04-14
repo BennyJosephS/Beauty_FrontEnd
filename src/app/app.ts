37777e9c7db11917a1561dea7ad88d2ea2811b07
BennyJosephS <bennyjoseph2609@gmail.com>
2026-04-14 12:42:13 +0530

    3resolve

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,23 +4,14 @@ import { Userhome } from "../userhome/userhome";
 import { Login } from './login/login';
 import { Cart } from './cart/cart';
 import { Adminhome } from './adminhome/adminhome';
-<<<<<<< HEAD
-import { Cart } from "./cart/cart";
 import { Orders } from "./orders/orders";
 import { RouterModule } from "@angular/router";
 import { AddProducts } from "./add-products/add-products";
+import { Return } from './return/return';
 
 @Component({
   selector: 'app-root',
   imports: [Login, Return, Register, Userhome, Adminhome, Cart, Orders, RouterModule, AddProducts],
-
-=======
-import { AddProduct } from "./add-product/add-product";
-
-@Component({
-  selector: 'app-root',
-  imports: [Register, Userhome, Adminhome, AddProduct, Login, Cart],
->>>>>>> a599f91f44240ff20bc165a43cb21e065c9cee3a
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
